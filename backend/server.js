b5cc71d0941070de39580587570d970d880aac2b
YukiMakoto25 <tamnd.22bi13400@usth.edu.vn>
2025-06-30 21:47:26 +0700

    Add log to server

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,10 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("/", (req, res) => {
+  res.send("Server is operational");
+});
+
 
 app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
