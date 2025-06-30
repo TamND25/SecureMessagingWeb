e5ec29faddf93a629505e1f81dee480dfa726129
YukiMakoto25 <tamnd.22bi13400@usth.edu.vn>
2025-07-01 01:48:10 +0700

    Change variable

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,9 @@ import { io } from "socket.io-client";
 
 const token = localStorage.getItem("token");
 
-const socket = io({
+const API_URL = process.env.REACT_APP_API_URL;
+
+const socket = io(API_URL, {
   auth: {
     token,
   },
